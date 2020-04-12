8b23a379bfce44b32eb943b2a5bf61fc681f3a1f
Mehmet Çağrı ÇALIŞKAN <mcagricaliskan@gmail.com>
2020-04-12 16:28:03 +0300

    Upload

---
+++ PyGame/Uygulama 2 ( Ders 8 - )/14 -.py
@@ -0,0 +1,194 @@
+import pygame
+import random
+
+pygame.init()
+
+class GameCore():
+    def __init__(self):
+        self.screen_width = 512
+        self.screen_height = 288
+        self.window = pygame.display.set_mode((self.screen_height, self.screen_width))
+        pygame.display.set_caption("Flappy Bird")
+        self.Clock = pygame.time.Clock()
+        self.GameStatus = "Start"
+
+        ####### BackGround ########
+        self.BackGroundDay = pygame.image.load("Data/sprites/background-day.png").convert_alpha()
+        self.BackGroundNight = pygame.image.load("Data/sprites/background-night.png").convert_alpha()
+        self.BackGroundList = [self.BackGroundDay, self.BackGroundNight]
+        self.BackGround = self.BackGroundList[random.randint(0, 1)]
+        self.BackGround_X = 0
+        self.BackGround_Y = 0
+
+        self.GameStartImage = pygame.image.load("Data/sprites/message.png").convert_alpha()
+
+        self.Ground = pygame.image.load("Data/sprites/base.png").convert_alpha()
+        self.Ground_X = 0
+        self.Ground_Y = 400
+        self.Ground_X2 = 336
+        self.Ground_Y2 = 400
+
+        ####### Bird ########
+
+        self.BirdUp = pygame.image.load("Data/sprites/yellowbird-upflap.png").convert_alpha()
+        self.BirdMid = pygame.image.load("Data/sprites/yellowbird-midflap.png").convert_alpha()
+        self.BirdDown = pygame.image.load("Data/sprites/yellowbird-downflap.png").convert_alpha()
+        self.BirdList = [self.BirdMid, self.BirdUp, self.BirdMid, self.BirdDown]
+        self.BirdMaskList = [
+            pygame.mask.from_surface(self.BirdMid),
+            pygame.mask.from_surface(self.BirdUp),
+            pygame.mask.from_surface(self.BirdMid),
+            pygame.mask.from_surface(self.BirdDown),
+
+        ]
+        self.BirdAnimation = 0
+        self.BirdAnimationDelay = 100
+        self.BirdAnimationTime = pygame.time.get_ticks()
+        self.Bird_X = 50
+        self.Bird_Y = 50
+        self.Gravity = 0.75
+        self.acc = 0.075
+
+        ##### PIPE ######
+        self.pipe_list = []
+        self.PIPEIMAGE = pygame.image.load("Data/sprites/pipe-green.png").convert_alpha()
+        self.PipeAnimationTime = pygame.time.get_ticks()
+        self.PipeAnimationDelay = 2000
+
+
+        self.Score = 0
+
+        self.ScorFont = pygame.font.SysFont("Arial", 40)
+
+
+
+    def MaskCollision(self,Masked_Image1, Image1_X, Image1_Y, Masked_Image2, Image2_X, Image2_Y):
+        offset = (round(Image2_X - Image1_X), round(Image2_Y - Image1_Y))
+        result = Masked_Image1.overlap(Masked_Image2, offset)
+        return result
+
+    class Pipe():
+        def __init__(self, pipe_X, pipe_Y, PipeImage):
+            self.pipe_X = pipe_X
+            self.pipe_Y = pipe_Y
+            self.lower_pipe = PipeImage
+            self.upper_pipe = pygame.transform.flip(self.lower_pipe, False, True)
+
+            self.lower_pipe_mask = pygame.mask.from_surface(self.lower_pipe)
+            self.upper_pipe_mask = pygame.mask.from_surface(self.upper_pipe)
+
+        def DrawPipe(self, window):
+            window.blit(self.lower_pipe, (self.pipe_X, self.pipe_Y))
+            window.blit(self.upper_pipe,(self.pipe_X, self.pipe_Y - 420))
+
+        def MovePipe(self):
+            self.pipe_X -= 1
+
+    def Draw(self):
+
+        self.window.blit(self.BackGround, (self.BackGround_X, self.BackGround_Y))
+
+
+        if self.GameStatus == "Start":
+            self.window.blit(self.GameStartImage, (50,50))
+        if self.GameStatus == "Game":
+
+            for pipe in self.pipe_list:
+                pipe.DrawPipe(self.window)
+            self.window.blit(self.BirdList[self.BirdAnimation], (self.Bird_X, self.Bird_Y))
+            self.window.blit(self.ScorFont.render("" + str(self.Score), True, (255, 255, 255)), (120, 50))
+
+        self.window.blit(self.Ground, (self.Ground_X, self.Ground_Y))
+        self.window.blit(self.Ground, (self.Ground_X2, self.Ground_Y2))
+
+        self.Clock.tick(60)
+
+
+        pygame.display.update()
+
+    def GameLoop(self):
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return "Close"
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if self.GameStatus == "Start":
+                        self.GameStatus = "Game"
+                    if self.GameStatus == "Game":
+                        self.Gravity = -2.8
+                        pygame.mixer.Channel(0).play(pygame.mixer.Sound("Data/audio/wing.ogg"))
+                    if self.GameStatus == "GameOver":
+                        self.GameStatus == "Start"
+
+        self.Tus = pygame.key.get_pressed()
+        if self.Tus[pygame.K_ESCAPE]:
+            return "Close"
+
+
+
+        if self.GameStatus == "Game":
+            ######### Gravity ###########
+            if self.Bird_Y < 380:
+                self.Bird_Y += self.Gravity
+                self.Gravity += self.acc
+
+            ######## Ground Control ######
+
+            self.Ground_X -= 1
+            self.Ground_X2 -= 1
+
+            if self.Ground_X == -336:
+                self.Ground_X = 336
+
+            if self.Ground_X2 == -336:
+                self.Ground_X2 = 336
+
+            ######### Animation #########
+            if pygame.time.get_ticks() - self.BirdAnimationTime > self.BirdAnimationDelay:
+                self.BirdAnimation += 1
+                if self.BirdAnimation == 4:
+                    self.BirdAnimation = 0
+                self.BirdAnimationTime = pygame.time.get_ticks()
+
+            ######## PIPE ###########
+            if pygame.time.get_ticks() - self.PipeAnimationTime > self.PipeAnimationDelay:
+                self.pipe_list.append(self.Pipe(300, random.randint(180,320), self.PIPEIMAGE))
+                self.PipeAnimationTime = pygame.time.get_ticks()
+
+            for pipe in self.pipe_list:
+                pipe.MovePipe()
+
+                collision_lower = self.MaskCollision(self.BirdMaskList[self.BirdAnimation],self.Bird_X,self.Bird_Y,
+                                   pipe.lower_pipe_mask,pipe.pipe_X,pipe.pipe_Y)
+                collision_upper = self.MaskCollision(self.BirdMaskList[self.BirdAnimation],self.Bird_X,self.Bird_Y,
+                                   pipe.upper_pipe_mask,pipe.pipe_X,pipe.pipe_Y - 420)
+
+                if collision_lower != None or collision_upper != None:
+                    pygame.mixer.Channel(2).play(pygame.mixer.Sound("Data/audio/hit.ogg"))
+                    self.pipe_list.clear()
+                    self.Bird_X = 50
+                    self.Bird_Y = 50
+                    self.Score = 0
+                    self.GameStatus = "Start"
+
+                if self.Bird_X + 17 == pipe.pipe_X:
+                    self.Score += 1
+                    pygame.mixer.Channel(1).play(pygame.mixer.Sound("Data/audio/point.ogg"))
+
+
+                if pipe.pipe_X == -52:
+                    self.pipe_list.remove(pipe)
+            print(self.Score)
+
+        self.Draw()
+
+
+Game = GameCore()
+
+while True:
+    GameStatus = Game.GameLoop()
+    if GameStatus == "Close":
+        break
+
+pygame.quit()
---
+++ PyGame/Uygulama 2 ( Ders 8 - )/SkorYazdırmaÖrnekli-FlappyBird.py
@@ -0,0 +1,222 @@
+import pygame
+import random
+
+pygame.init()
+
+class Game():
+    def __init__(self):
+        self.ScreenWidth = 288
+        self.ScreenHeight = 512
+        self.Window = pygame.display.set_mode((self.ScreenWidth, self.ScreenHeight))
+        pygame.display.set_caption("Flappy Bird")
+        self.clock = pygame.time.Clock()
+        self.BackGroundX = 0
+        self.BackGroundSecondX = 288
+        self.Time = pygame.time.get_ticks()
+        self.Time2 = pygame.time.get_ticks()
+        self.Time3 = pygame.time.get_ticks()
+        self.Time4 = pygame.time.get_ticks()
+        self.Time5 = pygame.time.get_ticks()
+        self.Delay = 50
+        self.DelayBase = 200
+        self.BirdDelay = 400
+        self.BaseX = 0
+        self.BaseSecondX = 336
+        self.PipeList = []
+        self.PipeDelay = 1250
+        self.BirdX = 50
+        self.BirdY = 200
+        self.Gravity = 0.75
+        self.acc = 0.075
+        self.Score = 0
+        self.GameStatus ="GameStart"
+        self.JumpVel = 35.75
+
+           
+           
+        # Yukarıdan gelen borular
+        # Zıplama
+        # Oyun modu
+
+
+
+
+        ####### SCORE SPRİTES ###########
+        self.Score0 = pygame.image.load("Data/sprites/0.png").convert_alpha()
+        self.Score1 = pygame.image.load("Data/sprites/1.png").convert_alpha()
+        self.Score2 = pygame.image.load("Data/sprites/2.png").convert_alpha()
+        self.Score3 = pygame.image.load("Data/sprites/3.png").convert_alpha()
+        self.Score4 = pygame.image.load("Data/sprites/4.png").convert_alpha()
+        self.Score5 = pygame.image.load("Data/sprites/5.png").convert_alpha()
+        self.Score6 = pygame.image.load("Data/sprites/6.png").convert_alpha()
+        self.Score7 = pygame.image.load("Data/sprites/7.png").convert_alpha()
+        self.Score8 = pygame.image.load("Data/sprites/8.png").convert_alpha()
+        self.Score9 = pygame.image.load("Data/sprites/9.png").convert_alpha()
+        self.ScoreList = [self.Score0, self.Score1, self.Score2, self.Score3, self.Score4, self.Score5,
+                          self.Score6, self.Score7, self.Score8, self.Score9]
+
+
+
+
+        ######## BİRD #########
+
+        self.BirdDown = pygame.image.load("Data/sprites/yellowbird-downflap.png").convert_alpha()
+        self.BirdMid = pygame.image.load("Data/sprites/yellowbird-midflap.png").convert_alpha()
+        self.BirdUp = pygame.image.load("Data/sprites/yellowbird-upflap.png").convert_alpha()
+        self.BirdList = [self.BirdMid, self.BirdUp, self.BirdMid, self.BirdDown]
+        self.BP = 0
+        self.PipeGreen = pygame.image.load("Data/sprites/pipe-green.png").convert_alpha()
+        self.PipeUpper = pygame.image.load("Data/sprites/pipe-green.png").convert_alpha()
+
+        self.Base = pygame.image.load("Data/sprites/base.png").convert_alpha()
+        self.BackGround = pygame.image.load("Data/sprites/background-day.png").convert()
+        self.GameOverImage = pygame.image.load("Data/sprites/gameover.png").convert_alpha()
+        self.GameStartImage = pygame.image.load("Data/sprites/message.png").convert_alpha()
+
+
+    def get_mask(self,Image1,Image1x,Image1y,Image2,Image2x,Image2y):
+        Image1_mask = pygame.mask.from_surface(Image1)
+        Image2_mask = pygame.mask.from_surface(Image2)
+        offset = (round(Image2x - Image1x), round(Image2y - Image1y))
+        result = Image1_mask.overlap(Image2_mask, offset)
+        return result
+
+
+    def Draw(self):
+        self.clock.tick(60)
+        self.Window.blit(self.BackGround, (self.BackGroundX, 0))
+        self.Window.blit(self.BackGround, (self.BackGroundSecondX, 0))
+        self.Window.blit(self.Base, (self.BaseX,400))
+        self.Window.blit(self.Base, (self.BaseSecondX, 400))
+        for Pipes in self.PipeList:
+            Pipes.PipeDraw(self.Window)
+
+        if self.GameStatus == "GameOver" or self.GameStatus == "Game":
+            if self.Score < 10:
+                self.Window.blit(self.ScoreList[self.Score],(132,20))
+            elif 10 <= self.Score < 100:
+                Score2 = list(str(self.Score))
+                Score21 = int(Score2[0])
+                Score22 = int(Score2[1])
+                self.Window.blit(self.ScoreList[Score21],(119,20))
+                self.Window.blit(self.ScoreList[Score22],(145,20))
+            else:
+                Score3 = list(str(self.Score))
+                self.Window.blit(self.ScoreList[int(Score3[0])], (114, 20))
+                self.Window.blit(self.ScoreList[int(Score3[1])], (132, 20))
+                self.Window.blit(self.ScoreList[int(Score3[2])], (158, 20))
+
+            self.Window.blit(self.BirdPicture, (self.BirdX, self.BirdY))
+
+        if self.GameStatus == "GameStart":
+            self.Window.blit(self.GameStartImage, (50,100))
+
+        if self.GameStatus == "GameOver":
+            self.Window.blit(self.GameOverImage,(50,200))
+
+        pygame.display.update()
+
+    class Pipe():
+        def __init__(self,PipeX,PipeY,PipeGreen,PipeUpper):
+            self.PipeX = PipeX
+            self.PipeY = PipeY
+            self.PipeGreen = PipeGreen
+            self.PipeUpper = PipeUpper
+            self.PiperUpperY = self.PipeY-420
+
+        def PipeDraw(self,Window):
+            Window.blit(self.PipeGreen,(self.PipeX,self.PipeY))
+            Window.blit(pygame.transform.flip(self.PipeUpper, False,True),(self.PipeX,self.PipeY-420))
+
+
+    def GameLoop(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return "Stop"
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and self.BirdY > 0:
+                    if self.GameStatus == "GameStart":
+                        self.BirdY = 200
+                        self.Gravity = 0.75
+                        self.GameStatus = "Game"
+                    if self.GameStatus == "Game":
+                        self.BirdY -= self.JumpVel
+                        self.Gravity = 0.75
+                        pygame.mixer.Channel(1).play(pygame.mixer.Sound("Data/audio/wing.ogg"))
+                    if self.GameStatus == "GameOver":
+                        self.PipeList.clear()
+                        self.BirdY = 200
+                        self.Gravity = 0.75
+                        self.Score = 0
+                        self.GameStatus = "GameStart"
+
+        ########## GRAVİTY ##########
+        if self.BirdY < 380:
+            self.BirdY += self.Gravity
+            self.Gravity += self.acc
+
+        self.Keys = pygame.key.get_pressed()
+        if self.Keys[pygame.K_ESCAPE]:
+            return "Stop"
+
+        if self.GameStatus == "Game":
+            self.BirdPicture = self.BirdList[self.BP]
+            if pygame.time.get_ticks() - self.Time5 > self.Delay:
+                self.BP+=1
+                if self.BP == 4:
+                    self.BP = 0
+                self.Time5 = pygame.time.get_ticks()
+
+            if self.BirdY > 375:
+                pygame.mixer.Channel(0).play(pygame.mixer.Sound("Data/audio/hit.ogg"))
+                self.GameStatus = "GameOver"
+            ########## ADDİNG PİPE ############
+            if pygame.time.get_ticks() - self.Time3 > self.PipeDelay:
+                self.PipeList.append(self.Pipe(288,random.randint(200,350),self.PipeGreen,self.PipeUpper))
+                self.Time3 = pygame.time.get_ticks()
+
+            if pygame.time.get_ticks() - self.Time2 > self.Delay:
+                self.BaseX -= 6
+                self.BaseSecondX -= 6
+                if self.BaseX < -335:
+                    self.BaseX = 0
+                if self.BaseSecondX < 1:
+                    self.BaseSecondX = 336
+
+                self.Time2 = pygame.time.get_ticks()
+
+            if pygame.time.get_ticks() - self.Time4 > self.Delay:
+                for PipeMove in self.PipeList:
+                    PipeMove.PipeX -= 6
+                    if self.BirdX + 16 == PipeMove.PipeX + 24:
+                        pygame.mixer.Channel(0).play(pygame.mixer.Sound("Data/audio/point.ogg"))
+                        self.Score += 1
+                        print(self.Score)
+                self.Time4 = pygame.time.get_ticks()
+
+            for Pipes in self.PipeList:
+                if Pipes.PipeX < -55:
+                    sıra = self.PipeList.index(Pipes)
+                    self.PipeList.pop(sıra)
+
+                self.Result = self.get_mask(self.BirdPicture,self.BirdX,self.BirdY,Pipes.PipeGreen,Pipes.PipeX,Pipes.PipeY)
+                self.Result2 = self.get_mask(self.BirdPicture, self.BirdX, self.BirdY, Pipes.PipeUpper, Pipes.PipeX, Pipes.PiperUpperY)
+                if self.Result or self.Result2:
+                    pygame.mixer.Channel(0).play(pygame.mixer.Sound("Data/audio/hit.ogg"))
+                    pygame.mixer.Channel(1).play(pygame.mixer.Sound("Data/audio/die.ogg"))
+                    self.GameStatus = "GameOver"
+
+
+
+
+        self.Draw()
+
+
+game = Game()
+
+while True:
+    Statu = game.GameLoop()
+    if Statu == "Stop":
+        break
+
+pygame.quit()
