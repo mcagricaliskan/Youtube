ff0a925d4b8b220e12564e07f1e55c66336031c5
Mehmet Çağrı ÇALIŞKAN <mcagricaliskan@gmail.com>
2020-04-28 21:39:32 +0300

    Update 01 - Metin Üretimi.py

--- Genetik Algoritması ve Neuroevolution (Python)/01 - Metin Üretimi/01 - Metin Üretimi.py
+++ Genetik Algoritması ve Neuroevolution (Python)/01 - Metin Üretimi/01 - Metin Üretimi.py
@@ -26,15 +26,64 @@ class Genetic:
         chromosome = [self.random_gene() for i in range(self.Target_Text_Lenght)]
         return chromosome
 
+    def calculate_fitness(self):
+        for Member in self.Population:
+            Member.Fitness = 0
+            for i in range(self.Target_Text_Lenght):
+                if Member.Chromosome[i] == self.Target[i]:
+                    Member.Fitness += 1
+
+            if Member.Fitness == self.Target_Text_Lenght:
+                self.Found_Text = Member.Chromosome
+                self.Found = True
+
+    def crossover(self):
+        last_best = int(( 90 * self.Population_Number) / 100)
+
+        self.Next_Generation = []
+        self.Next_Generation.extend(self.Population[last_best:])
+
+        while True:
+            if len(self.Next_Generation) < self.Population_Number:
+                member_1 = random.choice(self.Population[last_best:]).Chromosome
+                member_2 = random.choice(self.Population[last_best:]).Chromosome
+                new_member = []
+
+                for gene1,gene2 in zip(member_1, member_2):
+                    prob = random.random() # 0 - 1
+                    if prob < 0.47:
+                        new_member.append(gene1)
+                    elif prob < 0.94:
+                        new_member.append(gene2)
+                    else:
+                        new_member.append(self.random_gene())
+
+                self.Next_Generation.append(self.Member(new_member))
+
+
+            else:
+                break
+
+        self.Population = self.Next_Generation
+
+
+
     def main(self):
         for i in range(self.Population_Number):
             self.Population.append(self.Member(self.create_chromosome()))
 
+
         while not self.Found:
-            pass
+            self.calculate_fitness()
+            self.Population = sorted(self.Population, key=lambda Member: Member.Fitness)
+            self.crossover()
+            self.Generation_Timer += 1
+
+        print(f"You found = {self.Found_Text}, you did it {self.Generation_Timer} steps."
+              f"\n Your Target = {self.Target}")
 
 Target = "Mustafa Kemal Atatürk"
-Population_Number = 100
+Population_Number = 1000
 
 Go = Genetic(Target, Population_Number)
 Go.main()
